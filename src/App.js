47ef79c429cd6f0acb86af81340d45c877eabeff
Alex Perez-Davies <alexperezdavies@gmail.com>
2022-11-07 15:24:56 +0000

    Added NavBar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
-import {BrowserRouter, Routes, Route} from "react-router-dom"
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "./components/Layout.js";
+import SignUp from "./pages/SignUp.js";
+import Login from "./pages/Login.js";
+import AboutUs from "./pages/AboutUs.js";
+import Contact from "./pages/Contact.js";
+import MyProfile from "./pages/MyProfile.js";
+import CreateEvent from "./pages/CreateEvent.js";
 
 import { DataStore } from "@aws-amplify/datastore";
 import { USERS } from "./models";
@@ -22,10 +28,16 @@ function App() {
       <h1>Away Days</h1>
       <BrowserRouter>
         <Routes>
-          {/* <Route path="/create-event" element={<CreateEvent />}/> */}
+          <Route path="/" element={<Layout />}>
+            <Route path="/about-us" element={<AboutUs />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/my-profile" element={<MyProfile />} />
+            <Route path="/create-event" element={<CreateEvent />} />
+          </Route>
+          <Route path="/login" element={<Login />} />
+          <Route path="/sign-up" element={<SignUp />} />
         </Routes>
       </BrowserRouter>
-      
     </div>
   );
 }
