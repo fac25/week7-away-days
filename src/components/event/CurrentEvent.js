2850797947eb502ecb29b481076d5e27bb8a4512
Suraj Pun <s_pun@outlook.com>
2022-11-10 12:04:05 +0000

    profilePage

--- src/components/event/CurrentEvent.js
+++ src/components/event/CurrentEvent.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 
-const CurrentEvents = ({Events, DataStore}) => {
+const CurrentEvents = ({ Events, DataStore }) => {
   const [currentEvents, setCurrentEvents] = useState();
 
   useEffect(() => {
