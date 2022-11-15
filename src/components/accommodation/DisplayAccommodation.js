df3f304f748ceb39501b8973a78f98f0e0d952ca
patdel0 <zefiru@protonmail.com>
2022-11-15 11:00:26 +0000

    Add Image component

--- src/components/accommodation/DisplayAccommodation.js
+++ src/components/accommodation/DisplayAccommodation.js
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import { Accommodation } from "../../models";
 import { DataStore } from "aws-amplify";
+import Image from '../Image'
 
 const DisplayAccommodation = ({ eventId }) => {
   const [accommodation, setAccommodation] = useState();
@@ -23,7 +24,7 @@ const DisplayAccommodation = ({ eventId }) => {
         <div className="card">
           <h3>{accommodation.location}</h3>
           {accommodation.img.map((image, index) => (
-            <img key={`accommodation-image[$index]`} src={image} alt="test" />
+            <Image key={`accommodation-image[$index]`} src={image} alt="test" />
           ))}
           <p>Accommodation Description: {accommodation.description}</p>
           <p>Facilities: {accommodation.facilities?.join(", ")}</p>
