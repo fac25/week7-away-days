4c492aa187a50b727f2ca51783d014bad974d06e
patdel0 <zefiru@protonmail.com>
2022-11-15 12:58:00 +0000

    Make default value of `results` null

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -3,7 +3,7 @@ import Results from "../components/Results";
 import SearchBar from "../components/SearchBar";
 
 export default function Search() {
-  const [results, setResults] = useState([]);
+  const [results, setResults] = useState();
   return (
     <div>
       <SearchBar results={results} setResults={setResults} />
