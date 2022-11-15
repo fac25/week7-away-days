a77ac5509e57373c12659a4c9ecc9d79eaa16b79
Suraj Pun <s_pun@outlook.com>
2022-11-15 17:40:38 +0000

    Monday work

--- src/components/Results.js
+++ src/components/Results.js
@@ -1,8 +1,17 @@
 import formatDate from "../utils";
 import { Link } from "react-router-dom";
 import Image from "./Image";
+import { Events } from "../models";
+import { DataStore } from "aws-amplify";
 
 export default function Results({ results }) {
+  async function awsEventData() {
+    let awsEvents = await DataStore.query(Events);
+    console.log(awsEvents[0].name);
+
+    return <h4>{awsEvents[0].name}</h4>;
+  }
+
   return (
     <div>
       {results ? (
@@ -12,7 +21,10 @@ export default function Results({ results }) {
           <p>No events found...</p>
         )
       ) : (
-        ""
+        <>
+          <h1>Results</h1>
+          <button onClick={awsEventData}>Log Events</button>
+        </>
       )}
     </div>
   );
