805a57bd3228303a6c609d4f178dbc38596cf155
Alex Perez-Davies <alexperezdavies@gmail.com>
2022-11-16 12:22:47 +0000

    Retrieved my profile values in edit profile

--- src/components/profile/EditProfileComponent.js
+++ src/components/profile/EditProfileComponent.js
@@ -1,5 +1,5 @@
 import { DataStore } from "@aws-amplify/datastore";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Profile } from "../../models";
 import { Link } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
@@ -10,6 +10,19 @@ export default function EditProfileComponent({ user }) {
 
   const [profile, setProfile] = useState();
   const [profileImg, setProfileImg] = useState("");
+  const [values, setValues] = useState("");
+
+  useEffect(() => {
+    const currentUser = JSON.parse(localStorage.getItem("user"));
+    async function getCurrentValues() {
+      const profileValues = await DataStore.query(Profile, (item) =>
+        item.UserID("eq", currentUser.username)
+      );
+      console.log(profileValues);
+      setValues(profileValues[0]);
+    }
+    getCurrentValues();
+  }, []);
 
   const handleChange = (e) => {
     setProfile((prevState) => {
@@ -18,10 +31,9 @@ export default function EditProfileComponent({ user }) {
   };
 
   async function updateProfile() {
-    let original = await DataStore.query(Profile, (item) =>
+    const original = await DataStore.query(Profile, (item) =>
       item.UserID("eq", user.username)
     );
-
     await DataStore.save(
       Profile.copyOf(original[0], (updated) => {
         updated.UserID = user.username;
@@ -46,30 +58,70 @@ export default function EditProfileComponent({ user }) {
       <UploadImg updateFileName={setProfileImg} />
 
       <label htmlFor="name">name:</label>
-      <input type="text" id="name" onChange={handleChange} />
+      <input
+        type="text"
+        id="name"
+        onChange={handleChange}
+        defaultValue={values.name}
+      />
 
       <label htmlFor="lastName">last Name:</label>
-      <input type="text" id="lastName" onChange={handleChange} />
+      <input
+        type="text"
+        id="lastName"
+        onChange={handleChange}
+        defaultValue={values.lastName}
+      />
 
       <label htmlFor="about">About Me:</label>
-      <textarea type="text" id="about" onChange={handleChange} />
+      <textarea
+        type="text"
+        id="about"
+        onChange={handleChange}
+        defaultValue={values.about}
+      />
 
       <label htmlFor="favouriteSports">Favourite sports:</label>
-      <textarea type="text" id="favouriteSports" onChange={handleChange} />
+      <textarea
+        type="text"
+        id="favouriteSports"
+        onChange={handleChange}
+        defaultValue={values.favSports}
+      />
 
       <label htmlFor="why on away days">Why I'm on Away Days:</label>
-      <textarea type="text" id="why" onChange={handleChange} />
+      <textarea
+        type="text"
+        id="why"
+        onChange={handleChange}
+        defaultValue={values.whyOnAwayDays}
+      />
 
       <label htmlFor="one amazing sporting event">
         One amazing sporting event I've been to:
       </label>
-      <textarea type="text" id="oneSportingEvent" onChange={handleChange} />
+      <textarea
+        type="text"
+        id="oneSportingEvent"
+        onChange={handleChange}
+        defaultValue={values.oneAmazingSportEvent}
+      />
 
       <label htmlFor="past events">Events I've been to:</label>
-      <textarea type="text" id="pastEvents" onChange={handleChange} />
+      <textarea
+        type="text"
+        id="pastEvents"
+        onChange={handleChange}
+        defaultValue={values.pastEvents}
+      />
 
       <label htmlFor="interests">Interests:</label>
-      <textarea type="text" id="interests" onChange={handleChange} />
+      <textarea
+        type="text"
+        id="interests"
+        onChange={handleChange}
+        defaultValue={values.interests}
+      />
 
       <Link to="/my-profile" onClick={updateProfile}>
         Save
