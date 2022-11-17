fc279af1fea86d6c25d0473e5b264da825a27784
karwanm <karwan.ismo@gmail.com>
2022-11-17 11:36:06 +0000

    styles

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,26 +1,26 @@
 export default function Contact() {
   return (
-    <div class="get-in-touch" id="get-in-touch">
+    <div className="get-in-touch" id="get-in-touch">
                 <h2>Get In Touch</h2>
                 <form action="#">
-                    <div class="input-wrapper">
+                    <div className="input-wrapper">
                         <div>
-                            <label for="name">Name</label>
-                            <input type="text" class="name" name="name" id="name" placeholder="Name" required/>
+                            <label htmlFor="name">Name</label>
+                            <input type="text" className="name" name="name" id="name" placeholder="Name" required/>
                         </div>
                         <div>
-                            <label for="subject">Subject</label>
+                            <label htmlFor="subject">Subject</label>
                             <input className="subject" type="text" placeholder="Subject"/>
                         </div>
                     </div>
-                    <label for="email">Email</label>
-                    <input type="email" class="email" id="email" placeholder="Email" required/>
-                    <label for="message"> Message</label>
-                    <textarea class="message" name="message" id="message" placeholder="Your message ..."
+                    <label htmlFor="email">Email</label>
+                    <input type="email" className="email" id="email" placeholder="Email" required/>
+                    <label htmlFor="message"> Message</label>
+                    <textarea className="message" name="message" id="message" placeholder="Your message ..."
                         required></textarea>
-                    <div class="consent-wrapper">
+                    <div className="consent-wrapper">
                         <input id="consent" name="consent" type="checkbox" required />
-                        <label for="consent">I would like to receive future updates</label>
+                        <label htmlFor="consent">I would like to receive future updates</label>
                     </div>
                     <button type="submit" id="submit">Send message</button>
                 </form>
