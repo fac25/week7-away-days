aefcd803477fb44d5545799f7a2cafc108a208af
karwanm <karwan.ismo@gmail.com>
2022-11-17 11:32:09 +0000

    contact style

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,52 +1,29 @@
 export default function Contact() {
   return (
-    <div className="contact-us">
-      <div className="contact-us-wrapper">
-        <h2>Get In Touch</h2>
-        <form action="#">
-          <div className="input-wrapper">
-            <div>
-              <label htmlFor="name">Name</label>
-              <input
-                type="text"
-                className="name"
-                name="name"
-                id="name"
-                placeholder="Name"
-              />
+    <div class="get-in-touch" id="get-in-touch">
+                <h2>Get In Touch</h2>
+                <form action="#">
+                    <div class="input-wrapper">
+                        <div>
+                            <label for="name">Name</label>
+                            <input type="text" class="name" name="name" id="name" placeholder="Name" required/>
+                        </div>
+                        <div>
+                            <label for="subject">Subject</label>
+                            <input className="subject" type="text" placeholder="Subject"/>
+                        </div>
+                    </div>
+                    <label for="email">Email</label>
+                    <input type="email" class="email" id="email" placeholder="Email" required/>
+                    <label for="message"> Message</label>
+                    <textarea class="message" name="message" id="message" placeholder="Your message ..."
+                        required></textarea>
+                    <div class="consent-wrapper">
+                        <input id="consent" name="consent" type="checkbox" required />
+                        <label for="consent">I would like to receive future updates</label>
+                    </div>
+                    <button type="submit" id="submit">Send message</button>
+                </form>
             </div>
-            <div>
-              <label htmlFor="subject">Subject</label>
-              <input type="text"
-                className="subject"
-                name="subject"
-                id="subject"
-                placeholder="Subject"/>
-            </div>
-          </div>
-          <label htmlFor="email">Email</label>
-          <input
-            type="email"
-            className="email"
-            id="email"
-            placeholder="Email"
-          />
-          <label htmlFor="message"> Message</label>
-          <textarea
-            className="message"
-            name="message"
-            id="message"
-            placeholder="Your message ..."
-          ></textarea>
-          <div className="consent-wrapper">
-            <input id="consent" name="consent" type="checkbox" />
-            <label htmlFor="consent">I would like to receive future updates</label>
-          </div>
-          <button type="submit" id="submit">
-            Send message
-          </button>
-        </form>
-      </div>
-    </div>
   );
 }
